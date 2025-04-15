fca754d354335a84fedbf58e14d9aacff1d309ed
opensourcedev90s <opensourcedev90s@proton.me>
2025-04-15 19:38:55 +0100

    Decrease default limit

--- src/science_museum_mcp/constants.py
+++ src/science_museum_mcp/constants.py
@@ -4,5 +4,5 @@ SEARCH_OBJECTS_PATH = "search/objects"
 SEARCH_PEOPLE_PATH = "search/people"
 SEARCH_DOCUMENTS_PATH = "search/documents"
 
-DEFAULT_LIMIT = 100
+DEFAULT_LIMIT = 5 # Low due to LLM token constraints
 DEFAULT_OFFSET = 0
